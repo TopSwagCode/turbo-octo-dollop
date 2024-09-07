3350077add191e7efd881cca058431bdfa9b8089
Joshua Ryder <josh@topswagcode.com>
2024-09-07 19:49:14 +0200

    let people see what needs to be run first, before it works :D

--- CvMaker/Program.fs
+++ CvMaker/Program.fs
@@ -1,6 +1,9 @@
 ﻿namespace CvMaker
 
 module Main =
+
+    //let _ = Microsoft.Playwright.Program.Main([| "install" |]) // Used to install dependencies
+
     async {
         let applicant = DataStore.getAwesomeApplicant
     
